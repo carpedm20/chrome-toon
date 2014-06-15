0d2e2a27c91181212a76526fefbcc88ab2dfc3b1
Taehoon Kim <=>
2014-06-15 16:22:10 +0900

    raty... ha... need to convert to css

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,28 @@
 'use strict';
 
 var storage = chrome.storage.local;
-var webtoonApp = angular.module('webtoonApp', ['ngRoute']);
+
+var webtoonApp = angular.module('webtoonApp', ['ngRoute']).directive('myRepeatDirective', function() {
+  return function(scope, element, attrs) {
+    angular.element(element).css('color','blue');
+    if (scope.$last){
+      
+      $('.raty').raty(
+        { path: '/static/img',
+          readOnly: true,
+          score: function() {
+            console.log($(this).attr('score'));
+            return $(this).attr('score')/2;
+          }
+        });
+      }
+  };
+});
 
 var nBase = "http://comic.naver.com";
 var nmBase = "http://m.comic.naver.com"
 var nURL = nBase + "/webtoon/weekday.nhn"; // http://comic.naver.com/webtoon/weekday.nhn
 var nmURL = nmBase + "/webtoon/weekday.nhn?week="; // http://m.comic.naver.com/webtoon/weekday.nhn?week=mon
-
-var nComicUrl = nBase + "/webtoon/detail.nhn?titleId=";
 
 var isParsedComplete = false;
 
@@ -34,7 +48,56 @@ webtoonApp.config(function($routeProvider) {
 
 webtoonApp.controller('webtoonController', function($scope, $http) {
   /* Webtoon parser */
+  $scope.searchText = "";
   $scope.nList = {};
+  $scope.loading = true;
+  $scope.daum = true;
+  $scope.naver = true;
+  $scope.nComicUrl = nBase + "/webtoon/detail.nhn?titleId=";
+
+  $scope.openTab = function(link) {
+    chrome.tabs.create({ url: link });
+  }
+
+  $scope.refreshStar = function() {
+    $('.raty').raty(
+      { path: '/static/img',
+        readOnly: true,
+        score: function() {
+          return $(this).attr('score')/2;
+        }
+      });
+  }
+
+  $scope.titleFilter = function(toons) {
+    var naver = $("#naver").hasClass("checked");
+    console.log("naver : " + naver);
+    var daum = $("#daum").hasClass("checked");
+    console.log("daum : " + daum);
+
+    var result = {};
+    var previousLength = $(".toon-pub").length;
+
+    angular.forEach(toons, function(value, key) {
+      var toon = $scope.nList[key];
+
+      if (toon['title'].indexOf($scope.searchText) != -1) {
+        if(!naver) {
+          if (toon['pub']=="naver") {
+            result[key] = toon;
+          }
+        } else if (!daum) {
+          if (toon['pub']=="daum") {
+            result[key] = toon;
+          }
+        } else {
+          result[key] = toon;
+        }
+      }
+    });
+
+    return result;
+  }
 
   $scope.changeSubscription = function (id) {
     $scope.nList[id]['subscribe'] = !$scope.nList[id]['subscribe'];
@@ -46,63 +109,53 @@ webtoonApp.controller('webtoonController', function($scope, $http) {
     });
   }
 
-  $http.get(nURL).success(function(data) {
-    $(data).find('.thumb a').each(function(){
-      var img = $(this).find('img');
-      var day = $(this).attr('href').split('=')[2];
-      var id = $(this).attr('href').split('=')[1].split('&')[0];
+  setTimeout(function() {
+    $http.get(nURL).success(function(data) {
+      $(data).find('.thumb a').each(function(){
+        var img = $(this).find('img');
+        var day = $(this).attr('href').split('=')[2];
+        var id = $(this).attr('href').split('=')[1].split('&')[0];
 
-      if ($scope.nList[id] != undefined) {
-        $scope.nList[id]['days'].push(enToKoDay(day));
-      } else {
-        $scope.nList[id] = {
-          'thumbUrl': img.attr('src'),
-          'days': [ enToKoDay(day) ],
-          'title': img.attr('alt'),
-          'pub': 'naver',
-          'subscribe': false
-        };
-      }
-    });
+        if ($scope.nList[id] != undefined) {
+          $scope.nList[id]['days'].push(enToKoDay(day));
+        } else {
+          $scope.nList[id] = {
+            'thumbUrl': img.attr('src'),
+            'days': [ enToKoDay(day) ],
+            'title': img.attr('alt'),
+            'pub': 'naver',
+            'subscribe': false
+          };
+        }
+      });
 
-    storage.get('nList', function (obj) {
-      if (typeof obj['nList'] != 'undefined')
-        $scope.nList = obj['nList'];
-    });
+      $("#spinner").remove();
 
-    for (var day in enDay) {
-      $http.get(nmURL + enDay[day]).success(function(data) {
-        $(data).find('div.lst a').each(function(){
-          var id = $(this).attr('href').split('=')[1].split('&')[0];
-          var rating = $(this).find('span.st_r').text();
-          var author = $(this).find('span.sub_info').text();
+      storage.get('nList', function (obj) {
+        if (typeof obj['nList'] != 'undefined')
+          $scope.nList = obj['nList'];
+      });
 
-          if ($scope.nList[id] != undefined) {
-            $scope.nList[id]['rating'] = rating;
-            $scope.nList[id]['author'] = author;
-          } else {
-            alert("Error");
-          }
+      for (var day in enDay) {
+        $http.get(nmURL + enDay[day]).success(function(data) {
+          $(data).find('div.lst a').each(function(){
+            var id = $(this).attr('href').split('=')[1].split('&')[0];
+            var rating = $(this).find('span.st_r').text();
+            var author = $(this).find('span.sub_info').text();
 
-          /*$('#star-'+id).raty(
-            { path: '/static/img',
-              readOnly: true,
-              score: function() {
-                return $(this).attr('score')/2;
-              }
-            });*/
-        });
-      }).then(function(){
-        $('.raty').raty(
-          { path: '/static/img',
-            readOnly: true,
-            score: function() {
-              return $(this).attr('score')/2;
+            if ($scope.nList[id] != undefined) {
+              $scope.nList[id]['rating'] = rating;
+              $scope.nList[id]['author'] = author;
+            } else {
+              alert("Error");
             }
           });
-      });
-    }
-  });
+        }).then(function(){
+          $scope.refreshStar();
+        });
+      }
+    });
+  }, 1000);
 
   /*$http.get('/webtoones/webtoon_stops.json').success(function(data) {
     $scope.webtoonStops = data;
