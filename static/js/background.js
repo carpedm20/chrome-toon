aee9e464809e5d6a8d850963059d0ca210e50059
Taehoon Kim <=>
2014-06-17 13:21:24 +0900

    maybe almost finished

--- static/js/background.js
+++ static/js/background.js
@@ -3,6 +3,11 @@
  * reference: http://stackoverflow.com/questions/20326926/prevent-chrome-notifications-api-from-hiding-my-notification-after-a-few-seconds
  */
 
+var script = document.createElement('script');
+script.src = '/static/js/jquery-1.8.3.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 var storage = chrome.storage.local;
 var nList;
 
@@ -23,30 +28,64 @@ setInterval(function() {
     if (toon['subscribe'] == false)
       continue;
 
-    console.log(id);
+    $.ajax(
+      { url: 'http://comic.naver.com/webtoon/list.nhn?titleId=' + id,
+        success: function(data) {
+          var href = nBase + $(($(data).find(".title a"))[0]).attr('href');
+          var id = href.split('=')[1].split('&')[0];
 
-    var title = toon['title'];
-    var thumbUrl = toon['thumbUrl'];
+          storage.get(id, function (obj) {
+            if (typeof obj[id] != 'undefined') {
+              console.log("check : " + id);
 
-    var details = {
-      type:    "basic",
-      iconUrl: thumbUrl,
-      title:   title,
-      message: "우왕 떴닫!"
-    };
+              if (href != obj[id]) {
+                console.log("NEW : " + id + " - " + href);
 
-    chrome.notifications.create(id, details, function(notifId) {
-    });
+                var title = toon['title'];
+                var thumbUrl = toon['thumbUrl'];
+
+                console.log("thumb : " + title + " - " + thumbUrl);
+
+                var obj = {};
+                obj[id] = href;
+
+                storage.set(obj);
+
+                var details = {
+                  type:    "basic",
+                  iconUrl: thumbUrl,
+                  title:   title,
+                  message: "우왕 떴닫!"
+                };
+
+                chrome.notifications.create(id, details, function(notifId) {
+                  setTimeout(function() {
+                    destroyNotification(notifId);
+                  }, 3500);
+                });
+              }
+            } else {
+              var obj = {};
+              obj[id] = href;
+
+              storage.set(obj);
+            }
+          });
+        }
+      });
   }
 }, 10000);
 
+
 function destroyNotification(notifId) {
   chrome.notifications.clear(notifId, function(wasCleared) {
-    console.log('Destroyed notification "' + notifId + '" !');
+    console.log('Destroyed notification "' + notifId);
+
+    console.log("destroyed : " + notifId);
   });
 }
 
-/* Respond to the user's clicking on the notification message-body */
+/*
 chrome.notifications.onClicked.addListener(function(notifId) {
   notifId = notifId.toString();
   
@@ -54,9 +93,47 @@ chrome.notifications.onClicked.addListener(function(notifId) {
   destroyNotification(notifId);
 });
 
-/* Respond to the user's clicking on the small 'x' in the top right corner */
 chrome.notifications.onClosed.addListener(function(notifId, byUser) {
   notifId = notifId.toString();
 
   destroyNotification(notifId);
 });
+
+
+var title = "test";
+var thumbUrl = "http://thumb.comic.naver.net/webtoon/622648/thumbnail/title_thumbnail_20140511233526_t83x90.JPG";
+
+var details = {
+  type:    "basic",
+  iconUrl: thumbUrl,
+  title:   title,
+  message: "우왕 떴닫!"
+};
+
+chrome.notifications.create("123", details, function(notifId) {
+  setTimeout(function() {
+    destroyNotification(notifId);
+  }, 3500);
+});
+
+var notification = new Notification(title, {
+  body: "by ",
+  icon: thumbUrl
+});
+
+setTimeout(function() {
+  notification.close();
+}, 3500);
+
+
+var notification = new Notification(title, {
+  type: "basic",
+  body: "by ",
+  title: title,
+  icon: thumbUrl
+});
+
+setTimeout(function() {
+  notification.close();
+}, 3500);
+*/
